ef39c200b5e9203f347fe620e0f6372718d8568c
ShahbazCoder1 <shahbazhashmi14a@gmail.com>
2025-04-04 11:37:58 +0530

    index.dart setup done and onboarding page added

---
+++ ecopulse/lib/index.dart
@@ -0,0 +1,9 @@
+// Export pages
+export '/on_boarding/signin/signin_widget.dart' show SigninWidget;
+export '/on_boarding/signup/signup_widget.dart' show SignupWidget;
+export '/on_boarding/forgot/forgot_widget.dart' show ForgotWidget;
+export '/on_boarding/onboard/onboard_widget.dart' show OnboardWidget;
+export '/user/home/home_widget.dart' show HomeWidget;
+export '/user/report/report_widget.dart' show ReportWidget;
+export '/user/ecobot/ecobot_widget.dart' show EcobotWidget;
+export '/on_boarding/details/details_widget.dart' show DetailsWidget;
